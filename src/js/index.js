82c6575ab36db7689e5615d3c1e40b4dd0bb1897
ArisayMella <163840802+ArisayMella@users.noreply.github.com>
2024-06-27 11:58:49 +0200

    up

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const formElement = document.getElementById('form');
+
+const validateForm = event => {
+  event.preventDefault();
+};
+
+//console.log(isNan(4));
+
+//if (isNaN(Number('y')));
+//if (Number ('y') === NaN){
+//console.log('NO ES UN NUMERO');
+//}
+//
+
+formElement.addEventListener('submit', validateForm);
